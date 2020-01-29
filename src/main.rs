e8f32d8b1b19d6e91532563cf553137947494dfa
Kevin Wenzel <kevin.italo.wenzel@gmail.com>
2020-01-28 22:31:02 -0600

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rltk::{Rltk, GameState, Console};
+
+struct State{}
+impl GameState for State{
+	fn tick(&mut self, ctx : &mut Rltk){
+		ctx.cls();
+		ctx.print(1,1,"Hello Rust World");
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let context = Rltk::init_simple8x8(80,50,"Hello Rust World","resources");
+    let gs = State{};
+    rltk::main_loop(context, gs);
 }
